997555347c17ec78c157a689bd7c95779a30f87a
Harsh Shah <45804066+harsh007007007@users.noreply.github.com>
2020-11-21 02:00:55 +0530

    Update SJF.java

--- SJF.java
+++ SJF.java
@@ -3,91 +3,88 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package fcfs;
+package spn;
+
 import java.util.*; 
-/**
- *
- * @author ujjwa
- */
-public class SJF {
-    static int[][] mat = new int[10][6]; 
-  
-    static void arrangeArrival(int num, int[][] mat) { 
-        for (int i = 0; i < num; i++) { 
-            for (int j = 0; j < num - i - 1; j++) { 
-                if (mat[j][1] > mat[j + 1][1]) { 
-                    for (int k = 0; k < 5; k++) { 
-                        int temp = mat[j][k]; 
-                        mat[j][k] = mat[j + 1][k]; 
-                        mat[j + 1][k] = temp; 
-                    } 
-                } 
-            } 
-        } 
-    } 
-  
-    static void completionTime(int num, int[][] mat) { 
-        int temp, val = -1; 
-        mat[0][3] = mat[0][1] + mat[0][2]; 
-        mat[0][5] = mat[0][3] - mat[0][1]; 
-        mat[0][4] = mat[0][5] - mat[0][2]; 
-  
-        for (int i = 1; i < num; i++) { 
-            temp = mat[i - 1][3]; 
-            int low = mat[i][2]; 
-            for (int j = i; j < num; j++) { 
-                if (temp >= mat[j][1] && low >= mat[j][2]) { 
-                    low = mat[j][2]; 
-                    val = j; 
-                } 
-            } 
-            mat[val][3] = temp + mat[val][2]; 
-            mat[val][5] = mat[val][3] - mat[val][1]; 
-            mat[val][4] = mat[val][5] - mat[val][2]; 
-            for (int k = 0; k < 6; k++) { 
-                int tem = mat[val][k]; 
-                mat[val][k] = mat[i][k]; 
-                mat[i][k] = tem; 
-            } 
-        } 
-    } 
-  
-    // Driver Code 
-    public static void main(String[] args) { 
-        int num; 
-  
-        Scanner sc = new Scanner(System.in); 
-  
-        System.out.println("Enter number of Process: "); 
-        num = sc.nextInt(); 
-  
-        System.out.println("...Enter the process ID..."); 
-        for (int i = 0; i < num; i++) { 
-            System.out.println("...Process " + (i + 1) + "..."); 
-            System.out.println("Enter Process Id: "); 
-            mat[i][0] = sc.nextInt(); 
-            System.out.println("Enter Arrival Time: "); 
-            mat[i][1] = sc.nextInt(); 
-            System.out.println("Enter Burst Time: "); 
-            mat[i][2] = sc.nextInt(); 
-        } 
-  
-        System.out.println("Before Arrange..."); 
-        System.out.println("Process ID\tArrival Time\tBurst Time"); 
-        for (int i = 0; i < num; i++) { 
-            System.out.printf("%d\t\t%d\t\t%d\n",  
-                mat[i][0], mat[i][1], mat[i][2]); 
-        } 
-  
-        arrangeArrival(num, mat); 
-        completionTime(num, mat); 
-        System.out.println("Final Result..."); 
-        System.out.println("Process ID\tArrival Time\tBurst" + 
-                        " Time\tWaiting Time\tTurnaround Time"); 
-        for (int i = 0; i < num; i++) { 
-            System.out.printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\n",  
-             mat[i][0], mat[i][1], mat[i][2], mat[i][4], mat[i][5]); 
-        } 
-        sc.close(); 
-    } 
+
+ 
+public class Spn {
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println ("enter no of processes:");
+		int n = sc.nextInt();
+		int pid[] = new int[n];
+		int at[] = new int[n]; // at means arrival time
+		int bt[] = new int[n]; // bt means burst time
+		int ct[] = new int[n]; // ct means complete time
+		int ta[] = new int[n]; // ta means turn around time
+		int wt[] = new int[n];  //wt means waiting time
+		int f[] = new int[n];  // f means it is flag it checks process is completed or not
+		int st=0, tot=0;
+		float avgwt=0, avgta=0;
+ 
+		for(int i=0;i<n;i++)
+		{
+			System.out.println ("enter process " + (i+1) + " arrival time:");
+			at[i] = sc.nextInt();
+			System.out.println ("enter process " + (i+1) + " burst time:");
+			bt[i] = sc.nextInt();
+			pid[i] = i+1;
+			f[i] = 0;
+		}
+		
+		boolean a = true;
+		while(true)
+		{
+			int c=n, min=999;
+			if (tot == n) // total no of process = completed process loop will be terminated
+				break;
+			
+			for (int i=0; i<n; i++)
+			{
+				/*
+				 * If i'th process arrival time <= system time and its flag=0 and burst<min 
+				 * That process will be executed first 
+				 */ 
+				if ((at[i] <= st) && (f[i] == 0) && (bt[i]<min))
+				{
+					min=bt[i];
+					c=i;
+				}
+			}
+			
+			/* If c==n means c value can not updated because no process arrival time< system time so we increase the system time */
+			if (c==n) 
+				st++;
+			else
+			{
+				ct[c]=st+bt[c];
+				st+=bt[c];
+				ta[c]=ct[c]-at[c];
+				wt[c]=ta[c]-bt[c];
+				f[c]=1;
+				tot++;
+			}
+		}
+		
+                System.out.println("Processes " + 
+                           " Arrival time "+
+                           " Burst time " + 
+                           " Turnaround time "+
+                        " Waiting time ");
+                
+		for(int i=0;i<n;i++)
+		{
+			avgwt+= wt[i];
+			avgta+= ta[i];
+                        System.out.println(" " + pid[i] + "\t\t"+at[i]+"\t\t"
+                             + bt[i] + "\t\t "+ ta[i] + "\t\t "+wt[i]); 
+                            
+		}
+                System.out.println("Shortest Process Next:-");
+		System.out.println ("Average Turnaround time:"+ (float)(avgta/n));
+		System.out.println ("Average waiting time:"+ (float)(avgwt/n));
+		sc.close();
+	}
 }
