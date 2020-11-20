88f59779cf64b5855e8118a4e65678528162a401
Ujjwal N Shah <37004259+unshah@users.noreply.github.com>
2020-11-20 08:04:18 -0500

    Update RR.java

--- RR.java
+++ RR.java
@@ -4,131 +4,112 @@
  * and open the template in the editor.
  */
 package fcfs;
+import java.text.ParseException; 
+import java.util.*; 
 
 /**
  *
  * @author ujjwa
  */
-public class RR {
-    // Method to find the waiting time for all 
-    // processes 
-    static void findWaitingTime(int processes[], int n, 
-                 int bt[], int wt[], int quantum) 
-    { 
-        // Make a copy of burst times bt[] to store remaining 
-        // burst times. 
-        int rem_bt[] = new int[n]; 
-        for (int i = 0 ; i < n ; i++) 
-            rem_bt[i] =  bt[i]; 
-       
-        int t = 0; // Current time 
-       
-        // Keep traversing processes in round robin manner 
-        // until all of them are not done. 
-        while(true) 
-        { 
-            boolean done = true; 
-       
-            // Traverse all processes one by one repeatedly 
-            for (int i = 0 ; i < n; i++) 
-            { 
-                // If burst time of a process is greater than 0 
-                // then only need to process further 
-                if (rem_bt[i] > 0) 
-                { 
-                    done = false; // There is a pending process 
-       
-                    if (rem_bt[i] > quantum) 
-                    { 
-                        // Increase the value of t i.e. shows 
-                        // how much time a process has been processed 
-                        t += quantum; 
-       
-                        // Decrease the burst_time of current process 
-                        // by quantum 
-                        rem_bt[i] -= quantum; 
-                    } 
-       
-                    // If burst time is smaller than or equal to 
-                    // quantum. Last cycle for this process 
-                    else
-                    { 
-                        // Increase the value of t i.e. shows 
-                        // how much time a process has been processed 
-                        t = t + rem_bt[i]; 
-       
-                        // Waiting time is current time minus time 
-                        // used by this process 
-                        wt[i] = t - bt[i]; 
-       
-                        // As the process gets fully executed 
-                        // make its remaining burst time = 0 
-                        rem_bt[i] = 0; 
-                    } 
-                } 
-            } 
-       
-            // If all processes are done 
-            if (done == true) 
-              break; 
+public class FCFS {
+
+   // Function to find the waiting time for all  
+    // processes  
+    static void findWaitingTime(int processes[], int n, int at[],
+        int bt[], int wt[]) { 
+        // waiting time for first process is 0  
+        wt[0] = 0; 
+        int[] serv_time = new int[n];
+        
+        // calculating waiting time  
+        for (int i = 1; i < n; i++) { 
+            //Add service time for previous process
+            serv_time[i]= serv_time[i-1]+bt[i-1];
+            wt[i] = serv_time[i] - at[i]; 
         } 
     } 
-       
-    // Method to calculate turn around time 
-    static void findTurnAroundTime(int processes[], int n, 
-                            int bt[], int wt[], int tat[]) 
-    { 
-        // calculating turnaround time by adding 
-        // bt[i] + wt[i] 
-        for (int i = 0; i < n ; i++) 
+  
+    // Function to calculate turn around time  
+    static void findTurnAroundTime(int processes[], int n, int at[],
+            int bt[], int wt[], int tat[]) { 
+        // calculating turnaround time by adding  
+        // bt[i] + wt[i]  
+        for (int i = 0; i < n; i++) { 
             tat[i] = bt[i] + wt[i]; 
+        } 
     } 
-       
-    // Method to calculate average time 
-    static void findavgTime(int processes[], int n, int bt[], 
-                                         int quantum) 
-    { 
+  
+    //Function to calculate average time  
+    static void findavgTime(int processes[], int n, int at[], int bt[]) { 
         int wt[] = new int[n], tat[] = new int[n]; 
         int total_wt = 0, total_tat = 0; 
-       
-        // Function to find waiting time of all processes 
-        findWaitingTime(processes, n, bt, wt, quantum); 
-       
-        // Function to find turn around time for all processes 
-        findTurnAroundTime(processes, n, bt, wt, tat); 
-       
-        // Display processes along with all details 
-        System.out.println("Processes " + " Burst time " + 
-                      " Waiting time " + " Turn around time"); 
-       
-        // Calculate total waiting time and total turn 
-        // around time 
-        for (int i=0; i<n; i++) 
-        { 
+  
+        //Function to find waiting time of all processes  
+        findWaitingTime(processes, n, at, bt, wt); 
+  
+        //Function to find turn around time for all processes  
+        findTurnAroundTime(processes, n, at, bt, wt, tat); 
+  
+        //Display processes along with all details  
+        System.out.printf("Processes  Arrv time  Burst time  Waiting"
+                       +" time  Turn around time\n"); 
+  
+        // Calculate total waiting time and total turn  
+        // around time  
+        for (int i = 0; i < n; i++) { 
             total_wt = total_wt + wt[i]; 
             total_tat = total_tat + tat[i]; 
-            System.out.println(" " + (i+1) + "\t\t" + bt[i] +"\t " + 
-                              wt[i] +"\t\t " + tat[i]); 
+            System.out.printf("     %d    ", (i + 1)); 
+            System.out.printf("     %d    ", at[i]);
+            System.out.printf("     %d    ", bt[i]); 
+            System.out.printf("     %d      ", wt[i]); 
+            System.out.printf("     %d    \n", tat[i]); 
         } 
-       
-        System.out.println("Average waiting time = " + 
-                          (float)total_wt / (float)n); 
-        System.out.println("Average turn around time = " + 
-                           (float)total_tat / (float)n); 
+        float s = (float)total_wt /(float) n; 
+        int t = total_tat / n; 
+        System.out.printf("Average waiting time = %f", s); 
+        System.out.printf("\n"); 
+        System.out.printf("Average turn around time = %d ", t); 
     } 
-      
-    // Driver Method 
-    public static void main(String[] args) 
-    { 
-        // process id's 
-        int processes[] = { 1, 2, 3, 4, 5}; 
-        int n = processes.length; 
-       
-        // Burst time of all processes 
-        int burst_time[] = {10, 2, 5, 9, 7}; 
-       
-        // Time quantum 
-        int quantum = 2; 
-        findavgTime(processes, n, burst_time, quantum); 
-    }  
+  
+    // Driver code  
+    public static void main(String[] args) throws ParseException { 
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Number of Processes:");
+        int n = sc.nextInt();
+        System.out.println("Number of Processes:" + n);
+        //System.out.println("\n\nFirst Come First Serve:\n");        
+        
+        //process id's  
+        int[] processes = new int[n]; 
+        //int n = processes.length; 
+        int burst_time[] = new int[n];
+        int arr_time[] = new int [n];
+        //Burst time of all processes
+        for(int i = 0; i<n; i++){
+            int a = i+1;
+           
+            System.out.println("Arrival Time of Processes "+a+" :");
+            arr_time[i] = sc.nextInt();
+            System.out.println("Burst Time of Processe "+a+" :");
+            burst_time[i] = sc.nextInt();
+        }
+        //int burst_time[] = {10, 2, 5, 9, 7}; 
+        
+        findavgTime(processes, n,arr_time, burst_time); 
+        
+        System.out.println("\n\nRound Robin:\n");       
+        //RR.main(args);
+        
+        System.out.println("\n\nShortest Process Next:\n");     
+        //SJF.main(args);
+    
+        System.out.println("\n\nShortest Remaining Time First:\n");
+        SRTF.main(args);
+        
+        System.out.println("\n\nHighest Response Ratio Next:\n");
+        HRRN.main(args);
+    } 
+    
 }
